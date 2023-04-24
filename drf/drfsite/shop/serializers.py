a40e0cc2df2685f14f3b47df00cd6002eeb8f726
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-04-24 11:19:21 +0600

    serilazers

--- drf/drfsite/shop/serializers.py
+++ drf/drfsite/shop/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework import serializers
 from .models import Product
 
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
+class ProductSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=150)
+    slug = serializers.CharField()
+    image = serializers.ImageField()
+    description = serializers.TextField()
+    price = serializers.DecimalField()
+    available = serializers.BooleanField(default=True)
+    created = serializers.DateTimeField()
+    uploaded = serializers.DateTimeField()
+
+
+
+
+
+
+
+
+
+
+# class ProductModel:
+#     def __init__(self, name, description):
+#         self.name = name
+#         self.description = description
+        
+        
+# class ProductSerializer(serializers.ModelSerializer):
+#     name = serializers.CharField(max_length=230)
+#     description = serializers.CharField()
+    
+    
+# def encode():
+#     model = ProductModel()
+#     model_sr = ProductSerializer(model)
+    
--- drf/drfsite/shop/views.py
+++ drf/drfsite/shop/views.py
@@ -1,11 +1,27 @@
+from rest_framework.response import Response
 from rest_framework import generics
 from django.shortcuts import render
 from .models import Product, Category, Order
 from .serializers import ProductSerializer
+from .models import APIView
 
 from shop.models import Product
 
-class ProductAPIView(generics.ListAPIView):
-    queryset =Product.objects.all()
-    serializer_class = ProductSerializer
+
+class ProductAPIView(APIView):
+    def get(self, request):
+        lst = Product.objects.all(). values()
+        return Response({'posts': list(lst)})
+    
+    
+          
+          
+          
+
+# class ProductAPIView(generics.ListAPIView):
+#     queryset =Product.objects.all()
+#     serializer_class = ProductSerializer
+    
+    
+    #get - отвечает за обработку get запроосов
     
