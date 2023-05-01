7cea1c3318fe2a3bf70c2a61f6ec93e304f69e7f
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-01 12:10:27 +0600

    models, admin, reguarements.txt

--- example/calorite_tracker/food_consuming/models.py
+++ example/calorite_tracker/food_consuming/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Food(models.Model):
+    
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=100)
+    carbs = models.FloatField()
+    proteins = models.FloatField()
+    fats = models.FloatField()
+    calories = models.IntegerField()
+
+class Consume(models.Model):
+    user = models.ForeignKey(User)
+    food = models.ForeignKey(Food)
+    
+    
--- example/calorite_tracker/food_consuming/views.py
+++ example/calorite_tracker/food_consuming/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from food_consuming.models import Food, Consume
+from django.http import HttpResponse
 
-# Create your views here.
+
+def say_hello(request):
+    return HttpResponse("Hello food")
