72bad486abe82ab02e78a93413c38e9a7d7a709b
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-07 15:53:13 +0600

    advertisement_list.html

--- project/board/advertisement/views.py
+++ project/board/advertisement/views.py
@@ -3,10 +3,4 @@ from django.http import HttpResponse
 
 
 def advertisement_list(request, *args, **kwargs):
-    return HttpResponse(
-        '<ul>'
-        '<li>Что-то</li>'
-        '<li>Выведение из запроса</li>'
-        '<li>Услуги экскаватора</li>'
-        '</ul>'
-    )
+    return render(request, 'advertisement/advertisement_list.html', {})
