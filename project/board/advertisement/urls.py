13a5ed9481f943e7e5ff80efb127c1c084df636a
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-07 17:08:50 +0600

    basic class based vievs

--- project/board/advertisement/urls.py
+++ project/board/advertisement/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.advertisement_list, name='advertisement_list')
+    path('', views.advertisement_list, name='advertisement_list'),
+    path('about/', views.About.as_view())
 ]
--- project/board/advertisement/views.py
+++ project/board/advertisement/views.py
@@ -1,6 +1,7 @@
+from typing import Any, Dict
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.views import View
+from django.views.generic import TemplateView
 
 def advertisement_list(request, *args, **kwargs):
     advertisements = [
@@ -8,3 +9,14 @@ def advertisement_list(request, *args, **kwargs):
         'что-то'
     ]
     return render(request, 'advertisement/advertisement_list.html', {'adverisements':advertisements})
+
+
+class About (TemplateView):
+    template_name = 'advertisement/about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'Бесплатные объявление в вашем городе'
+        context['title'] = 'Бесплатные объявление'
+        context['description'] = """Вы хотите что-то"""
+        return context
