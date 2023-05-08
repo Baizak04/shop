7307c9f6e2345383bef43b4406266f5c5de11619
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-08 20:06:42 +0600

    models.py

--- project/board/advertisement/models.py
+++ project/board/advertisement/models.py
@@ -9,6 +9,16 @@ class Advertisement(models.Model):
     updated = models.DateTimeField(auto_now=True)
     price = models.FloatField(verbose_name='цена',default=0)
     views_count = models.IntegerField(verbose_name='количество просмотров',default=0)
+    status = models.ForeignKey('AdvertisementStatus', default=None, null=True, on_delete=models.CASCADE,
+                               related_name='advertisements')
     
     def __str__(self):
         return self.title
+    
+    class Meta:
+        db_table = 'advertisements'
+        ordering = ['title']
+    
+class AdvertisementStatus(models.Model):
+    name = models.CharField(max_length=100)
+    
