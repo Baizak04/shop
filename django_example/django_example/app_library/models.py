bdcfebbfb328ae42b8366a969fecef76ac6df394
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-10 19:50:23 +0600

    admin.ModelAdmin editing related posts

--- django_example/django_example/app_library/models.py
+++ django_example/django_example/app_library/models.py
@@ -7,10 +7,24 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     is_active = models.BooleanField()
     
+    def __str__(self):
+        return f'{self.id}, {self.name} ({self.country}, {self.city})'
+    
     
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
     biography = models.TextField()
+    
+    def __str__(self):
+        return f'{self.id}, {self.first_name} {self.last_name}'
+    
+    
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    
     
