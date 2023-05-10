a5a45813b281b3587a55f4d25dbfdd0607ed5443
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-10 19:22:59 +0600

    admin.ModelAdmin filters and search box

--- django_example/django_example/app_library/models.py
+++ django_example/django_example/app_library/models.py
@@ -6,3 +6,11 @@ class Publisher(models.Model):
     city = models.CharField(max_length=60)
     country = models.CharField(max_length=50)
     is_active = models.BooleanField()
+    
+    
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    biography = models.TextField()
+    
