727d02951b849397889087d99ed6770f9f763e89
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-05-13 15:44:52 +0600

    video #6 itproger

--- web-app/itproger/main/urls.py
+++ web-app/itproger/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
-    path('index_two/', views.index_two),
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('index_two/', views.index_two, name='index_two'),
 ]
