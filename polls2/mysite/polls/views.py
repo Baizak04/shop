a0f54132fe6389681a4521e34540452b320f1f95
Baizak31052004 <nadurbekovbaizak@gmail.com>
2023-07-04 12:34:39 +0600

    add code polls the end part3

--- polls2/mysite/polls/views.py
+++ polls2/mysite/polls/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
 
-def salam(request):
-    return HttpResponse("<u>Salam Python off 7</u><b>salam</b>")
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "Вы смотрите на результаты вопроса %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Вы голосуете по вопросу %s." % question_id)
